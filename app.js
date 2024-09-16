7d280ca98cb93b27540ee075aadeb2040579bd9c
matthew-fitzgerald123 <mattfitz0421@icloud.com>
2024-09-16 02:34:07 -0400

    changed some settings in the repo

--- app.js
+++ app.js
@@ -19,3 +19,4 @@ app.get('/contact', (req, res) => res.render('contact'));
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
